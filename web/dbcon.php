f7bae25ccbbca02a7287fd64680ba5096f16c78e
ashwan0805 <125804456+ashwan0805@users.noreply.github.com>
2024-08-22 13:56:15 +0530

    Update dbcon.php

--- web/dbcon.php
+++ web/dbcon.php
@@ -1,3 +1,12 @@
 <?php
 include 'dbcon.php';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cse";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 ?>
